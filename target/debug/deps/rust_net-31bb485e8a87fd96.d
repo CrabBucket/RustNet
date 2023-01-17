e678bc9a9d495b2900c899d5ab32414e77ead826
CrabBucket <tom@mcratracing.com>
2023-01-17 08:40:25 -0800

    Delete target directory

--- target/debug/deps/rust_net-31bb485e8a87fd96.d
+++
@@ -1,7 +0,0 @@
-c:\Users\CrabD\Projects\RustNet\target\debug\deps\rust_net-31bb485e8a87fd96.rmeta: src\main.rs src\neuralnet\mod.rs src\neuralnet\layer.rs
-
-c:\Users\CrabD\Projects\RustNet\target\debug\deps\rust_net-31bb485e8a87fd96.d: src\main.rs src\neuralnet\mod.rs src\neuralnet\layer.rs
-
-src\main.rs:
-src\neuralnet\mod.rs:
-src\neuralnet\layer.rs:
